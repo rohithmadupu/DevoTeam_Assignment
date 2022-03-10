7074dc5c419fb66445bc0a459050c5923acfac6f
rohithmadupu <101269459+rohithmadupu@users.noreply.github.com>
2022-03-11 02:03:57 +0530

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -4,8 +4,6 @@
 # ### EDA
 # The Notebook gives an EDA of the dataset provided. We are looking at the transactions provided in the dataset and hightlight any issues in the data.
 
-# In[1]:
-
 
 ###Importing Libraries required
 import pandas as pd
@@ -15,22 +13,15 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-# In[2]:
-
-
 ###Path for the url at which the data is saved
 csv_url = "https://raw.githubusercontent.com/rohithmadupu/DevoTeam_Assignment/main/ecommerce_data.csv"
 
-
-# In[3]:
 
 
 ###reading the csv and displaying head
 df_ecomm = pd.read_csv(csv_url)
 df_ecomm.head()
 
-
-# In[4]:
 
 
 ###Getting schema of the dataframe imported
@@ -47,15 +38,11 @@ df_ecomm["CustomerID"] = df_ecomm["CustomerID"].str.replace('.0', '')
 df_ecomm["Estore_id"] = df_ecomm["Estore_id"].astype("str")
 
 
-# In[6]:
 
 
 ####Checking if both Invoice Dates match and then removing one
 if (df_ecomm['InvoiceDate'].equals(df_ecomm['InvoiceDate.1']) == True):
     df_ecomm = df_ecomm.drop(["InvoiceDate.1"], axis = 1)
-
-
-# In[7]:
 
 
 ###creating a datefield and year field from Invoice Date
@@ -64,38 +51,22 @@ df_ecomm['Date'] = pd.to_datetime(df_ecomm.InvoiceDate,errors='coerce').dt.date
 df_ecomm['Year'] = pd.to_datetime(df_ecomm.InvoiceDate,errors='coerce').dt.year
 
 
-# In[8]:
-
 
 ###Getting dimensions of the Dataframe
 df_ecomm.shape
 
 
-# In[9]:
-
-
 ###Min and Max dates for which the data is available
 f"Min and Max dates are {df_ecomm['Date'].min()} and {df_ecomm['Date'].max()}"
-
-
-# In[10]:
-
 
 ###Count of nulls by Column
 print(df_ecomm.isnull().sum(axis=0))   # Around 120k nulls in Stockcode,desc and quantity. No nulls in others
 
 
-# In[11]:
-
-
 ###count of negative values in Quantity and Unit Price 
 print(f"Negative Values in Quantity Column are {(df_ecomm['Quantity'] < 0).sum()}\n       Negative Values in UnitPrice Column are {(df_ecomm['UnitPrice'] < 0).sum()}")
 
-####No nulls in unit price
-
-
-# In[12]:
-
+###Comment - No nulls in unit price
 
 #### Getting number of transactions by Date
 df_ecomm_cnt = pd.DataFrame(df_ecomm.groupby('Date').size().rename('Count').reset_index())
@@ -103,23 +74,15 @@ df_ecomm_cnt.sort_values(by=['Count'], ascending=False)
 #--most transactions on 2011-October-6th and least on 2011-Feb-06
 
 
-# In[13]:
-
-
 ###Unique values by column excluding Nans
 
 df_ecomm.dropna().nunique()
-
-
-# In[14]:
 
 
 ### Line plot - Count of transactions by Date
 plt.figure(figsize = (15,8))
 sns.lineplot(x = 'Date', y = 'Count',data = df_ecomm_cnt)
 
-
-# In[15]:
 
 
 ###Number of Transactions by Year
@@ -136,8 +99,6 @@ plt.figure(figsize = (15,8))
 )
 
 
-# In[16]:
-
 
 ###Number of transactions by Store 
 df_store_cnt = df_ecomm.groupby(['Estore_id']).size().rename('Count').reset_index()
@@ -151,8 +112,6 @@ plt.figure(figsize = (15,8))
                 palette = "Blues_r")
 )
 
-
-# In[17]:
 
 
 ###Number of transactions by Customer ID 
@@ -168,8 +127,6 @@ plt.figure(figsize = (15,8))
 )
 
 
-# In[18]:
-
 
 ###Number of transactions by Invoice number
 df_inv_cnt = df_ecomm.groupby(['InvoiceNo']).size().rename('Count').reset_index()
@@ -183,8 +140,6 @@ plt.figure(figsize = (15,8))
                 palette = "Blues_r")
 )
 
-
-# In[19]:
 
 
 ###Number of transactions by Country
@@ -200,8 +155,6 @@ plt.figure(figsize = (15,8))
 )
 
 
-# In[20]:
-
 
 ###Number of transactions by StockCode
 df_stock_cnt = df_ecomm.groupby(['StockCode']).size().rename('Count').reset_index()
@@ -215,8 +168,6 @@ plt.figure(figsize = (15,8))
                 palette = "Blues_r")
 )
 
-
-# In[21]:
 
 
 ###negative values by Date
