fbd32c0aa1f4f15b007877e6a9a90cceac147ebb
rohithmadupu <101269459+rohithmadupu@users.noreply.github.com>
2022-03-11 02:08:21 +0530

    Update Top N Analysis.py

--- Top N Analysis.py
+++ Top N Analysis.py
@@ -1,11 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 # ### Top N analysis
 # The purpose of this notebook is to identify the top e-stores that contribute to the revenue of GloboSales. In addition,  top countries for GloboSales to focus on and their top e-stores are identified. For the analysis, the rows where quantity is negative have been removed 
-
-# In[1]:
-
 
 ###Importing Libraries required
 import pandas as pd
@@ -16,14 +10,8 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import PercentFormatter
 
 
-# In[2]:
-
-
 ###Path for the url at which the data is saved
 csv_url = "https://raw.githubusercontent.com/rohithmadupu/DevoTeam_Assignment/main/ecommerce_data.csv"
-
-
-# In[3]:
 
 
 ###reading the csv and displaying head
@@ -31,18 +19,11 @@ df_ecomm = pd.read_csv(csv_url)
 df_ecomm.head()
 
 
-# In[4]:
-
-
 ##Converting customer Id and estore ID to string
 df_ecomm["CustomerID"] = df_ecomm["CustomerID"].astype("str")
 df_ecomm["CustomerID"] = df_ecomm["CustomerID"].str.replace('.0', '')
 
 df_ecomm["Estore_id"] = df_ecomm["Estore_id"].astype("str")
-
-
-# In[5]:
-
 
 ####Checking if both Invoice Dates match and then removing one
 if (df_ecomm['InvoiceDate'].equals(df_ecomm['InvoiceDate.1']) == True):
@@ -54,13 +35,7 @@ df_ecomm['Date'] = pd.to_datetime(df_ecomm.InvoiceDate,errors='coerce').dt.date
 df_ecomm['Year'] = pd.to_datetime(df_ecomm.InvoiceDate,errors='coerce').dt.year
 
 
-# In[6]:
-
-
 print(df_ecomm.isnull().sum(axis=0))
-
-
-# In[7]:
 
 
 ###Removing nulls and negative values from Quantity Column from analysis 
@@ -68,28 +43,15 @@ print(df_ecomm.isnull().sum(axis=0))
 
 df_ecomm_new = df_ecomm[df_ecomm['Quantity'] >= 0]
 
-
-# In[8]:
-
-
 df_ecomm_new.head() ### 
-
-
-# In[9]:
 
 
 ###Creating a Revenue column
 df_ecomm_new["Revenue"] = df_ecomm_new.Quantity * df_ecomm_new.UnitPrice
 
 
-# In[10]:
-
-
-###Revenue by EstoreId
+##Revenue by EstoreId
 df_estore_rev = pd.DataFrame(df_ecomm_new.groupby(["Estore_id"]).agg({'Revenue': 'sum'}).reset_index())
-
-
-# In[11]:
 
 
 plt.figure(figsize = (15,10))
@@ -100,13 +62,9 @@ sns.barplot(x = 'Estore_id',
             data = df_estore_rev,
             palette = 'Blues_r')
  
-# Show the plot
 plt.show()
 
 ###Top 5 Estores are 1,31,4,30,17
-
-
-# In[12]:
 
 
 df = pd.DataFrame(df_estore_rev[["Revenue"]])
@@ -114,9 +72,6 @@ df.index = df_estore_rev["Estore_id"]
 df = df.sort_values(by='Revenue',ascending=False)
 df["cumpercentage"] = df["Revenue"].cumsum()/df["Revenue"].sum()*100
 df
-
-
-# In[13]:
 
 
 fig = plt.figure(figsize=(100,80), dpi=55)
@@ -131,16 +86,10 @@ ax.tick_params(axis="y", colors="C0")
 ax2.tick_params(axis="y", colors="C1")
 
 
-# In[14]:
-
-
 ###Identifyng top customers for estores 1,31,4
 df_estore_cust_rev = pd.DataFrame(df_ecomm_new.groupby(["Estore_id","CustomerID"]).agg({'Revenue': 'sum'}).reset_index())
 
 df_estore_cust_rev = df_estore_cust_rev[df_estore_cust_rev.CustomerID != "nan"]
-
-
-# In[47]:
 
 
 ###estore1
@@ -152,9 +101,6 @@ df_es1 = (
 )
 
 
-# In[49]:
-
-
 plt.figure(figsize = (10,7))
 
 sns.barplot(x = 'CustomerID',
@@ -162,11 +108,7 @@ sns.barplot(x = 'CustomerID',
             data = df_es1,
             hue = "Estore_id")
  
-# Show the plot
 plt.show()
-
-
-# In[51]:
 
 
 ###estore2
@@ -178,9 +120,6 @@ df_es2 = (
 )
 
 
-# In[52]:
-
-
 plt.figure(figsize = (10,7))
 
 sns.barplot(x = 'CustomerID',
@@ -188,11 +127,7 @@ sns.barplot(x = 'CustomerID',
             data = df_es2,
             hue = "Estore_id")
  
-# Show the plot
 plt.show()
-
-
-# In[53]:
 
 
 ###estore3
@@ -204,9 +139,6 @@ df_es3 = (
 )
 
 
-# In[54]:
-
-
 plt.figure(figsize = (10,7))
 
 sns.barplot(x = 'CustomerID',
@@ -214,19 +146,12 @@ sns.barplot(x = 'CustomerID',
             data = df_es3,
             hue = "Estore_id")
  
-# Show the plot
 plt.show()
-
-
-# In[20]:
 
 
 ###Identifyng top stocks for estores 1,31,4
 df_estore_stock_rev = pd.DataFrame(df_ecomm_new.groupby(["Estore_id","StockCode"]).agg({'Revenue': 'sum'}).reset_index())
 df_estore_stock_rev = df_estore_stock_rev[df_estore_stock_rev.StockCode != "nan"]
-
-
-# In[56]:
 
 
 ##estore1
@@ -238,9 +163,6 @@ df_es_stk1 = (
 )
 
 
-# In[57]:
-
-
 plt.figure(figsize = (10,7))
 
 sns.barplot(x = 'StockCode',
@@ -248,11 +170,7 @@ sns.barplot(x = 'StockCode',
             data = df_es_stk1,
             hue = "Estore_id")
  
-# Show the plot
 plt.show()
-
-
-# In[59]:
 
 
 ###estore2
@@ -264,9 +182,6 @@ df_es_stk2 = (
 )
 
 
-# In[61]:
-
-
 plt.figure(figsize = (10,7))
 
 sns.barplot(x = 'StockCode',
@@ -274,11 +189,7 @@ sns.barplot(x = 'StockCode',
             data = df_es_stk2,
             hue = "Estore_id")
  
-# Show the plot
 plt.show()
-
-
-# In[60]:
 
 
 ###estore3
@@ -289,10 +200,6 @@ df_es_stk3 = (
     .head(n= 10))
 )
 
-
-# In[62]:
-
-
 plt.figure(figsize = (10,7))
 
 sns.barplot(x = 'StockCode',
@@ -300,18 +207,10 @@ sns.barplot(x = 'StockCode',
             data = df_es_stk3,
             hue = "Estore_id")
  
-# Show the plot
 plt.show()
 
-
-# In[26]:
-
-
-###Revenue by Country
+ ###Revenue by Country
 df_country_rev = pd.DataFrame(df_ecomm_new.groupby(["Country"]).agg({'Revenue': 'sum'}).reset_index())
-
-
-# In[27]:
 
 
 plt.figure(figsize = (15,10))
@@ -322,14 +221,9 @@ sns.barplot(x = 'Country',
             data = df_country_rev,
             palette = 'Blues_r')
  
-# Show the plot
 plt.show()
 
 ###Top 5 Countries are UK,Germany,France,EIRE,Netherlands
-
-
-# In[63]:
-
 
 df_cntry = pd.DataFrame(df_country_rev[["Revenue"]])
 df_cntry.index = df_country_rev["Country"]
@@ -353,14 +247,9 @@ plt.show()
 ###4 Countries UK,Germany,France,EIRE are contributing to 80% revenue -> goal is to focus more on them
 
 
-# In[65]:
-
-
 ###Identifyng top 3 Estores and customers for Countries UK,Germany,France,EIRE
 df_country_estore_rev = pd.DataFrame(df_ecomm_new.groupby(["Country","Estore_id"]).agg({'Revenue': 'sum'}).reset_index())
 
-
-# In[66]:
 
 
 ####Identifying top e-stores associated with each country
@@ -373,9 +262,6 @@ df_uk_estore = (
 )
 
 
-# In[67]:
-
-
 plt.figure(figsize = (10,7))
 
 sns.barplot(x = 'Estore_id',
@@ -383,11 +269,7 @@ sns.barplot(x = 'Estore_id',
             data = df_uk_estore,
             hue = "Country")
  
-# Show the plot
 plt.show()
-
-
-# In[68]:
 
 
 ##Germany
@@ -399,9 +281,6 @@ df_de_estore = (
 )
 
 
-# In[69]:
-
-
 plt.figure(figsize = (10,7))
 
 sns.barplot(x = 'Estore_id',
@@ -409,12 +288,7 @@ sns.barplot(x = 'Estore_id',
             data = df_de_estore,
             hue = "Country")
  
-# Show the plot
 plt.show()
-
-
-# In[70]:
-
 
 
 ###France
@@ -426,9 +300,6 @@ df_fr_estore = (
 )
 
 
-# In[71]:
-
-
 plt.figure(figsize = (10,7))
 
 sns.barplot(x = 'Estore_id',
@@ -436,12 +307,7 @@ sns.barplot(x = 'Estore_id',
             data = df_fr_estore,
             hue = "Country")
  
-# Show the plot
 plt.show()
-
-
-# In[72]:
-
 
 
 ###EIRE
@@ -453,9 +319,6 @@ df_ir_estore = (
 )
 
 
-# In[73]:
-
-
 plt.figure(figsize = (10,7))
 
 sns.barplot(x = 'Estore_id',
@@ -463,6 +326,5 @@ sns.barplot(x = 'Estore_id',
             data = df_ir_estore,
             hue = "Country")
  
-# Show the plot
 plt.show()
 
